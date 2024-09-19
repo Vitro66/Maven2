20293dacbb5742e3328a769d1ab3b1db00eb8dee
Vitro66 <v.maneiro.r@gmail.com>
2024-09-19 10:22:50 +0200

    Add files via upload

---
+++ src/test/java/grupo1/art/Ejercicio7ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo1.art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
